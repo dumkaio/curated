fec2a6cf5d3ae631692fd3a9d1ada2300728f38b
thealexlebed <87195408+thealexlebed@users.noreply.github.com>
2021-07-20 16:17:59 +0300

    Update index-1.0.5.js

--- index-1.0.5.js
+++ index-1.0.5.js
@@ -156,7 +156,7 @@ $('#email-form3').on('submit', function () {
   $('#email-form4').on('submit', function () {
     const id = $(this).attr('id');
     $('.w-form-done, .w-form-fail').hide();
-    const select = $(`#${id} input[type=select]`).text();
+    const select = $(`#${id} input[type=select]`).val();
     let url = `https://curated.chilipiper.com/book/me/caitlin-stower?select=${select}`;
     window.open(url);
   });
