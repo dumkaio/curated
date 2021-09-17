b29c0464f7ec130c0f905a8f7c9bae6c0c2d76e6
Oleksandr Telnov <otelnov@gmail.com>
2021-09-17 18:02:35 +0300

    gulp min and email validator

--- index.js
+++ index.js
@@ -143,14 +143,14 @@ $(function () {
     const formId = $(this).closest('form').attr('id');
     $(`#${formId}`).submit();
   });
-  
+
   //referral form
   $('#add-inputs-1').on('click', function () {
-    $('#referral-form .btn').removeClass( "btn--disable" )
+    $('#referral-form .btn').removeClass('btn--disable');
   });
   $('#referral-form .btn').on('click', function () {
     const formId = $(this).closest('form').attr('id');
-    if (!($(this).hasClass( "btn--disable" ))) {
+    if (!$(this).hasClass('btn--disable')) {
       $(`#${formId}`).submit();
     }
   });
@@ -174,9 +174,8 @@ $(function () {
         url += `&${_utm_campaign}`;
       }
       window.open(url);
-    } 
+    }
   });
-  
 
   $('#email-form, #email-form2').on('submit', function () {
     const id = $(this).attr('id');
@@ -837,3 +836,27 @@ $(function () {
     });
   }
 });
+
+// merch page
+$(function () {
+  if (location.pathname === '/merch') {
+    $('#merchEmail1, #merchEmail2, #merchEmail3').on('keyup', function () {
+      const id = $(this).attr('id');
+      $(`#${id}-error`).hide();
+    });
+    $('#merchEmail1, #merchEmail2, #merchEmail3').on('focusout', function () {
+      const id = $(this).attr('id');
+      const val = $(this).val();
+      $(`#${id}-error`).hide();
+      if (!isBusinessEmail(val)) {
+        $(`#${id}-error`).show();
+      }
+    });
+  }
+});
+
+function isBusinessEmail(email) {
+  var regex =
+    /^([\w-+\.]+@(?!gmail.com)(?!yahoo.com)(?!hotmail.com)(?!aol.com)(?!hotmail.co.uk)(?!hotmail.fr)(?!msn.com)(?!yahoo.fr)(?!wanadoo.fr)(?!orange.fr)(?!comcast.net)(?!yahoo.co.uk)(?!yahoo.com.br)(?!yahoo.co.in)(?!live.com)(?!rediffmail.com)(?!free.fr)(?!gmx.de)(?!web.de)(?!yandex.ru)(?!ymail.com)(?!libero.it)(?!outlook.com)(?!uol.com.br)(?!bol.com.br)(?!mail.ru)(?!cox.net)(?!hotmail.it)(?!sbcglobal.net)(?!sfr.fr)(?!live.fr)(?!verizon.net)(?!live.co.uk)(?!googlemail.com)(?!yahoo.es)(?!ig.com.br)(?!live.nl)(?!bigpond.com)(?!terra.com.br)(?!yahoo.it)(?!neuf.fr)(?!yahoo.de)(?!alice.it)(?!rocketmail.com)(?!att.net)(?!laposte.net)(?!facebook.com)(?!bellsouth.net)(?!yahoo.in)(?!hotmail.es)(?!charter.net)(?!yahoo.ca)(?!yahoo.com.au)(?!rambler.ru)(?!hotmail.de)(?!tiscali.it)(?!shaw.ca)(?!yahoo.co.jp)(?!sky.com)(?!earthlink.net)(?!optonline.net)(?!freenet.de)(?!t-online.de)(?!aliceadsl.fr)(?!virgilio.it)(?!home.nl)(?!qq.com)(?!telenet.be)(?!me.com)(?!yahoo.com.ar)(?!tiscali.co.uk)(?!yahoo.com.mx)(?!voila.fr)(?!gmx.net)(?!mail.com)(?!planet.nl)(?!tin.it)(?!live.it)(?!ntlworld.com)(?!arcor.de)(?!yahoo.co.id)(?!frontiernet.net)(?!hetnet.nl)(?!live.com.au)(?!yahoo.com.sg)(?!zonnet.nl)(?!club-internet.fr)(?!juno.com)(?!optusnet.com.au)(?!blueyonder.co.uk)(?!bluewin.ch)(?!skynet.be)(?!sympatico.ca)(?!windstream.net)(?!mac.com)(?!centurytel.net)(?!chello.nl)(?!live.ca)(?!aim.com)(?!bigpond.net.au)([\w-]+\.)+[\w-]{2,4})?$/;
+  return regex.test(email);
+}
